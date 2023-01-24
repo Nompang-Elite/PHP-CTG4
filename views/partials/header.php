d1c533eb26334e2d386db7212111ef17245ee420
ShitaroKhido <khidpagna01@gmail.com>
2023-01-24 15:19:17 +0700

    [FILES]: Updated

--- views/partials/header.php
+++ views/partials/header.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="../css/style.css">
-    <title>header</title>
     <title><?=$header?></title>
 </head>
 <body>
