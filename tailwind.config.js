7952cee7b7544eae6244508606830341b99403b9
ShitaroKhido <khidpagna01@gmail.com>
2023-01-24 10:36:57 +0700

    [FILES]: Update file content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["views\\Home\\home.view.php"],
+  content: ["views\\Home\\home.view.php", "views\\Search\\search.view.php"],
   theme: {
     extend: {},
   },
