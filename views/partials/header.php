3292b2c357dfef2adcc1502b0a934e4cf39dc859
ShitaroKhido <khidpagna01@gmail.com>
2023-01-24 15:12:15 +0700

    [FILES]: Updated

--- views/partials/header.php
+++ views/partials/header.php
@@ -4,12 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="../css/style.css">
     <title>header</title>
-=======
     <title><?=$header?></title>
->>>>>>> f4e85aa59182132283b0c86fa7eb998948dcafa3
 </head>
 <body>
