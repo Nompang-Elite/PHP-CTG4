a5c5243fa7c5d8db2e73da0d292fcfb9c48972cb
ShitaroKhido <khidpagna01@gmail.com>
2023-01-26 06:49:06 +0700

    [CTG-85]: Edited Login controller

--- controllers/login/login.controller.php
+++ controllers/login/login.controller.php
@@ -3,7 +3,9 @@
 $configure = require("utils/config.php");
 if ($_SERVER["REQUEST_METHOD"] === "POST" and (isset($_POST["email"]) and isset($_POST["password"]))) {
     if (!empty($_POST["email"]) and !empty($_POST["password"])) {
+        $pattern = '/[a-z]+@+[a-z!0-9].{3,4}/';
         echo $_POST["email"] . " " . $_POST["password"];
+        echo preg_match($pattern, $_POST["email"]);
     } else
         routeToPage("/", $configure["route"]);
 } else
---
+++ models/login.model.php
@@ -0,0 +1,6 @@
+<?php
+
+function login($db, $email, $pass): bool
+{
+    
+}
