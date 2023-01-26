56bfe0debe93d555991e75e2ee27dddaaa482974
ShitaroKhido <khidpagna01@gmail.com>
2023-01-26 10:13:05 +0700

    [CTG-85]: Fixed login controller

--- controllers/login/login.controller.php
+++ controllers/login/login.controller.php
@@ -14,13 +14,10 @@ $mydb = new Database($config["databaseInfo"]);
 
 // Login page controller:
 if ($_SERVER["REQUEST_METHOD"] === "POST" and (isset($_POST["email"]) and isset($_POST["password"]))) {
-    if (!empty($_POST["email"]) and !empty($_POST["password"])) {
-        if (validateUserLogin($mydb, $_POST["email"], $_POST["password"]) !== false) {
-            header("Location: /");
-        }
+    if (!empty($_POST["email"]) and !empty($_POST["password"]) and (validateUserLogin($mydb, $_POST["email"], $_POST["password"]) !== false)) {
+        header("Location: /");
     } else {
         $incorrect = "border-b-red-400";
-        $notValid = true;
         require("views/html/login/login.view.php");
     }
 } else
