231d13da3d17825de9d104a50bba5989d4644119
ShitaroKhido <khidpagna01@gmail.com>
2023-01-28 11:54:45 +0700

    [CTG-45]: Created the list show.

--- models/list_show.model.php
+++ models/list_show.model.php
@@ -2,7 +2,8 @@
 
 function getTheAvailableShow(Database $db)
 {
-    $statement = $db->query("SELECT * FROM Tickets INNER JOIN Shows");
-    $list = $statement->fetch();
+    $statement = $db->query("SELECT * FROM Shows;");
+    $list = $statement->fetchAll();
+    // dump_die($list); // Use this to debugs
     return $list;
 }
