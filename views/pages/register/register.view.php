d95a19d54c042ab38ea1b3d56341a5464bdb6ba6
ShitaroKhido <khidpagna01@gmail.com>
2023-01-28 10:16:21 +0700

    [CTG-48]: Update views and controller.

--- views/pages/register/register.view.php
+++ views/pages/register/register.view.php
@@ -16,29 +16,29 @@ require("views/partials/header.php");
             </div>
             <!-- Register Form -->
             <div class="flex bg-white items-center justify-center flex-col shadow-md border-l-2 w-[36rem] min-w-[20rem] max-sm:w-full">
-                <form action="/register" method="post" id="login-form" class="w-full flex flex-col items-center gap-[1rem]">
+                <form action="/register" method="post" id="register-form" class="w-full flex flex-col items-center gap-[1rem]">
                     <legend class="text-3xl font-bold">Register</legend>
                     <!-- Name Input -->
                     <div class="flex gap-[1rem] max-w-8/10 ">
-                        <input type="text" name="firstName" id="firstName" class="rounded-md p-2 text-center border-2 w-full" placeholder="First Name ">
-                        <input type="text" name="lastName" id="lastName" class="rounded-md p-2 text-center border-2 w-full" placeholder=" last name ">
+                        <input type="text" name="firstName" id="firstName" class="outline-none rounded-md p-2 text-center border-2 w-full" placeholder="First Name ">
+                        <input type="text" name="lastName" id="lastName" class="outline-none rounded-md p-2 text-center border-2 w-full" placeholder=" last name ">
                     </div>
                     <!-- Username Input -->
-                    <input type="text" name="username" id="username" class="max-w-8/10  rounded-md p-2 text-center border-2 w-full" placeholder="Phone Number">
+                    <input type="text" name="username" id="username" class="outline-none max-w-8/10 rounded-md p-2 text-center border-2 w-full" placeholder="Username">
                     <!-- Email Input -->
-                    <input type="email" name="email" id="email" class="max-w-8/10  rounded-md p-2 text-center border-2 w-full" placeholder="Email Address">
+                    <input type="email" name="email" id="email" class="outline-none max-w-8/10  rounded-md p-2 text-center border-2 w-full" placeholder="Email Address">
                     <!-- Password Input -->
-                    <input type="password" name="createPass" id="createPass" class="max-w-8/10  rounded-md p-2 text-center border-2 w-full" placeholder="Create Password">
-                    <input type="password" name="comfirmPass" id="comfirmPass" class="max-w-8/10  rounded-md p-2 text-center border-2 w-full" placeholder="Confirm Password ">
+                    <input type="password" name="createPass" id="createPass" class="outline-none max-w-8/10  rounded-md p-2 text-center border-2 w-full" placeholder="Create Password">
+                    <input type="password" name="comfirmPass" id="comfirmPass" class="outline-none max-w-8/10  rounded-md p-2 text-center border-2 w-full" placeholder="Confirm Password ">
                     <!-- Gender Input -->
-                    <select id="gender" name="gender" class="max-w-8/10 rounded-md p-2 text-center border-2 w-full">
+                    <select id="gender" name="gender" class="outline-none max-w-8/10 rounded-md p-2 text-center border-2 w-full">
                         <option selected disabled>Gender</option>
                         <option value="US">Male</option>
                         <option value="CA">Female</option>
                     </select>
                     <!-- Date Input -->
                     <div class="flex gap-[1rem] w-full max-w-8/10">
-                        <input type="date" name="birthDate" id="birthDate" class="w-full rounded-md p-2 text-center border-2">
+                        <input type="date" name="birthDate" id="birthDate" class="outline-none w-full rounded-md p-2 text-center border-2">
                         <!-- Country Input -->
                         <select id="" name="city" class="w-full rounded-md p-2 text-center border-2">
                             <option selected disabled>Country</option>
@@ -62,3 +62,4 @@ require("views/partials/header.php");
         </div>
     </div>
 </main>
+<script src="/views/js//controllers/register.js"></script>
