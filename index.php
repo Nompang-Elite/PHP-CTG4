6b132f475f8564acc973c1388347df67b927412d
ShitaroKhido <khidpagna01@gmail.com>
2023-01-30 15:02:12 +0700

    [UPDATED]: Config

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
-
+// Import config:
+$config = require_once("utils/config.php");
+// Import debug:
 require_once("utils/debug.php");
+// Import Database:
+require_once("database/Database.php");
+// Import router:
 require_once("router.php");
-require_once("");
+// Getting URI path from server
+$uri = $_SERVER["REQUEST_URI"];
+// Routing Page to based on URI
 routeToPage($uri, $config["route"]);
--- utils/config.php
+++ utils/config.php
@@ -1,8 +1,25 @@
 <?php
 return [
-    "route" => [],
+    "route" => [
+        "/" => "controllers/home/home.controller.php",
+        "" => "",
+    ],
     "database" => [
-        "online" => [],
-        "offline" => []
+        // Online Database Connection
+        "online" => [
+            "host" => "php-database.c9wmosguks3w.ap-southeast-1.rds.amazonaws.com",
+            "dbname" => "ticket_app",
+            "user" => "admin",
+            "pass" => "Iseeyou1234",
+            "port" => "3306",
+        ],
+        // Offline Database Connection
+        "offline" => [
+            "host" => "",
+            "dbname" => "",
+            "user" => "",
+            "pass" => "",
+            "port" => "",
+        ]
     ]
 ];
