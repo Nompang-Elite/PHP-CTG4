bdd50fbdd48edd078eb8e804d33ce949d442958d
ShitaroKhido <khidpagna01@gmail.com>
2023-01-30 13:23:21 +0700

    [RECONSTRUCTION]

--- controllers/search/search.controller.php
+++
@@ -1,7 +0,0 @@
-<?php
-$config = require("utils/config.php");
-// Establish database connection:
-// $db = new Database($config["databaseInfoLocal"]);
-$db = new Database($config["databaseInfoOnline"]);
-$shows = getTheAvailableShow($db);
-require("views/pages/search/search.view.php");
--- models/validation.model.php
+++
@@ -1,9 +0,0 @@
-<?php
-function sanitizeValue(array $array): array
-{
-    $sanitized = [];
-    foreach ($array as $key => $value) {
-        $sanitized[$key] = htmlspecialchars($value);
-    };
-    return $sanitized;
-}
