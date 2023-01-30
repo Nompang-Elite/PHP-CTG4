67eee813ff4b5ba9e353776d99c407b1980e3866
ShitaroKhido <khidpagna01@gmail.com>
2023-01-30 16:16:16 +0700

    [UPDATED]: Users model.

--- controllers/home/home.controller.php
+++ controllers/home/home.controller.php
@@ -1,19 +1,3 @@
 <?php
 $header = "Home";
-
-// Import config files:
-$config = require("utils/config.php");
-// Establish database connection:
-// $db = new Database($config["databaseInfoLocal"]);
-$db = new Database($config["database"]["online"]);
-// Check if the user is logged in:
-$user = checkUserLogin($db);
-
-if (isset($_SESSION["user"])) {
-    $userInfo = $_SESSION["user"];
-    $username = "{$userInfo["username"]}";
-    // Use this when to check if data transfer correctly:
-    // dump_die($userInfo);
-}
-$showsList = getTheAvailableShow($db);
 require("views/pages/home/home.view.php");
--- controllers/login/login.controller.php
+++ controllers/login/login.controller.php
@@ -1,22 +1,15 @@
 <?php
-// Import config files:
-$config = require("utils/config.php");
-// Establish database connection:
-$db = new Database($config["database"]["online"]);
-// Create a user object:
-$user = new Users($db);
-// Validate the login:
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (!empty($_POST["email"]) and !empty($_POST["password"])) {
-        if ($user->login($_POST["email"], $_POST["password"])) {
-            // dump_die($user);
-            header("Location: /");
-        }
+    if ($_POST["email"] &&  $_POST["password"]) {
+        $config = require("utils/config.php");
+        $db = new Database($config["database"]["online"]);
+        $user = new User($db);
+        $validateLogin = $user->login($_POST["email"], $_POST["password"]);
     }
 }
 
-if (parse_url($_SERVER["REQUEST_URI"])["path"] === "/logout") {
-    $user->logout();
-    routeToPage("/", $config["route"]);
-} else
-    require("views/pages/login/login.view.php");
+
+
+
+require("views/pages/login/login.view.php");
