80309426e79fd0a37c95e7c0eb57702f95f2691d
ShitaroKhido <khidpagna01@gmail.com>
2023-02-03 08:16:06 +0700

    [CTG-140]: Added header code.

---
+++ views/partials/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google Material Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <!-- Tailwindcss -->
+    <link rel="stylesheet" href="../../contents/themes/css/tw-style.css">
+    <!-- Costume Style -->
+    <link rel="stylesheet" href="../../contents/themes/css/main.min.css">
+    <!-- JQeury -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js" defer></script>
+    <!-- JS -->
+    <script src="../../scripts/main.js" defer></script>
+    <title>Home</title>
+</head>
+
+<body>
