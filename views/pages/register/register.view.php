3c714074661770ea5bd50ae93ff8dd295a8812f6
SoriyaTY <soriya.hor@student.passerellesnumeriques.org>
2023-02-04 18:56:30 +0700

    [CTG-135]: update style

--- views/pages/register/register.view.php
+++ views/pages/register/register.view.php
@@ -4,12 +4,13 @@ require './views/partials/header.php';
 <main class="w-full h-screen  flex justify-center items-center bg-slate-900 text-white">
     <div class="flex h-full w-full">
         <div class="flex justify-center items-center  w-full max-sm:hidden ">
+            <!-- Contents -->
             <img src="https://wallpaperaccess.com/full/436209.jpg" alt="logo" class="relative h-full box-content opacity-10">
             <div class="relative h-full w-full">
 
             </div>
             <div class="absolute flex items-center flex-col">
-                <img src="../../../contents/imgs/logo.png" alt="" width="60%" class="">
+                <img src="./images/logo.png" alt="" width="60%" class="">
                 <div class="p-4">
                     <h1 class="text-2xl ">Welcome to my website!</h1>
                     <p class="text-xs">The world is so beautiful. Keep your best. Think positive.</p>
@@ -57,17 +58,19 @@ require './views/partials/header.php';
             </label>
             <label class="flex w-full flex-col">
                 <label class="ml-9">Email</label>
-                <input type="email" name="email" onkeyup="validateEmailRegister()" id="email" placeholder="Email address" class="p-2 rounded-md max-w-8/10 ml-9 w-10/12 bg-transparent outline-none bg-gray-800">
+                <input type="email" name="email" onkeyup="validateEmail()" id="email" placeholder="Email address" class="p-2 rounded-md max-w-8/10 ml-9 w-10/12 bg-transparent outline-none bg-gray-800">
                 <span id="text" class="ml-9 text-sm"></span>
             </label>
             <label class="flex w-full flex-row">
                 <label class="flex w-full flex-col">
                     <label class="ml-9">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Password" class="p-2 rounded-md max-w-8/10 ml-9 w-9/12 bg-transparent outline-none bg-gray-800">
+                    <input type="password" name="password" id="password" onkeyup="validatePassword()" placeholder="Password" class="p-2 rounded-md max-w-8/10 ml-9 w-9/12 bg-transparent outline-none bg-gray-800">
+                    <span id="textpwd" class="ml-9 text-sm"></span>
                 </label>
                 <label class="flex w-full flex-col">
                     <label class="ml-5">Confirm_Password</label>
                     <input type="password" name="confirmpassword" id="confirmpassword" placeholder="Confirm_Password" class="p-2 rounded-md max-w-8/10 ml-5 w-9/12 bg-transparent outline-none bg-gray-800">
+                    <span id="textconfirmpassword" class="ml-9 text-sm"></span>
                 </label>
             </label>
             <button class="btn p-2 mt-4 rounded-md max-w-8/10 w-10/12 border-2 border-[#01c8ee] text-[#01c8ee] ">Login</button>
