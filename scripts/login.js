1f44df0cb273d79196bdd9f53d7294214b26fd87
PHANNITPHUOY <phannitphuoy1102@gmail.com>
2023-02-06 16:28:43 +0700

    [CTG-141]: Update style

--- scripts/login.js
+++ scripts/login.js
@@ -1,21 +1,23 @@
 function validateEmail()
     {
         let form = document.getElementById("login-form");
-        let email = document.getElementById("email").value;
+        let email = document.getElementById("email");
         let text= document.getElementById("text");
         let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
         
-        if (email.match(emailPattern)){
+        if (email.value.match(emailPattern)){
             form.classList.add("valid");
             form.classList.remove("invalid")
             text.innerHTML="Valid email!";
             text.style.color="green";
-            completed++;
+            email.style.border = "2px solid green";
+
         }else{
             form.classList.remove("valid");
             form.classList.add("invalid");
             text.innerHTML="Invalid email!";
             text.style.color="red";
+            email.style.border = "2px solid red";
         }
         if(email==""){
             form.classList.remove("valid");
@@ -28,18 +30,21 @@ function validateEmail()
 function validPassoword()
     {
         let form = document.getElementById("login-form");
-        let password = document.getElementById("pass").value;
+        let password = document.getElementById("pass");
         let textPassword= document.getElementById("textPassword");
-        if(password=="")
+        if(password.value=="")
         { 
             form.classList.add("valid");
             form.classList.remove("invalid")
             textPassword.innerHTML="Please! Enter password.";
             textPassword.style.color="red";
+            password.style.border = "2px solid red";
+            
         }else{
             form.classList.remove("valid");
             form.classList.remove("invalid");
             textPassword.innerHTML="";
+            password.style.border = "none";
             
         }
     }
