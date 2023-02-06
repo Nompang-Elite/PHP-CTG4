462cf14ad1857bc777d07021332a00efe2cfc7cd
SoriyaTY <soriya.hor@student.passerellesnumeriques.org>
2023-02-06 11:44:08 +0700

    [CTG-143]: add confirm pwd function

--- scripts/form.validation.js
+++ scripts/form.validation.js
@@ -1,41 +1,68 @@
 function validateEmail() 
 {
-  let email = document.getElementById("email");
-  let text = document.getElementById("text");
-  let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    let email = document.getElementById("email");
+    let text = document.getElementById("text");
+    let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-  if (email.value.match(emailPattern)) {
-    text.innerHTML = "Please Enter Valid Email!";
-    text.style.color = "green";
-    email.style.border = "2px solid green";
-    completed++;
-  } else {
-    text.innerHTML = "Invalid Email!";
-    text.style.color = "red";
-    email.style.border = "2px solid red";
-  }
-  if (email.value == "") {
-    text.innerHTML = "";
-  }
+    if (email.value.match(emailPattern)) {
+        text.innerHTML = "Please Enter Valid Email!";
+        text.style.color = "green";
+        email.style.border = "2px solid green";
+        completed++;
+    } else {
+        text.innerHTML = "Invalid Email!";
+        text.style.color = "red";
+        email.style.border = "2px solid red";
+    }
+    if (email.value == "") {
+        text.innerHTML = "";
+    }
+
 }
 
 function validatePassword() 
 {
-  let password = document.getElementById("password");
-  let textPassword = document.getElementById("textpwd");
-  let passPattern =
-    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
-  if (password.value.match(passPattern)) {
-    textPassword.innerHTML = "Valid Password!";
-    textPassword.style.color = "green";
-    password.style.border = "2px solid green";
-    completed++;
-  } else {
-    textPassword.innerHTML = "Please Enter Valid Password!";
-    textPassword.style.color = "red";
-    password.style.border = "2px solid red";
-  }
-  if (password.value == "") {
-    textPassword.innerHTML = "";
-  }
+    let password = document.getElementById("password");
+    let textPassword = document.getElementById("textpwd");
+    let passPattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    if (password.value.match(passPattern)) {
+        textPassword.innerHTML = "Valid Password!";
+        textPassword.style.color = "green";
+        password.style.border = "2px solid green";
+        completed++;
+    } 
+    else 
+    {
+        textPassword.innerHTML = "Please Enter Valid Password!";
+        textPassword.style.color = "red";
+        password.style.border = "2px solid red";
+    }
+    if (password.value == "") 
+    {
+        textPassword.innerHTML = "";
+    }
+}
+
+function validateConfirmPassword()
+{
+    let password = document.getElementById("password").value;
+    let textconfirmPassword = document.getElementById("textconfirmpassword");
+    let confirmPassword = document.getElementById("confirmpassword");
+    if (confirmPassword.value == password)
+    {
+        textconfirmPassword.innerHTML = "Valid Password!";
+        textconfirmPassword.style.color = "green";
+        confirmPassword.style.border = "2px solid green";
+        completed++;
+    }
+    else
+    {
+        textconfirmPassword.innerHTML = "Please Enter Valid Password!";
+        textconfirmPassword.style.color = "red";
+        confirmPassword.style.border = "2px solid red";
+    }
+    if (password.value == "") 
+    {
+        textPassword.innerHTML = "";
+    }
 }
