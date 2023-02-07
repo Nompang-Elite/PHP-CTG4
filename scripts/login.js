f87b51f8f00c8f2bf4fb26c6f402ae83bda1570c
PHANNITPHUOY <phannitphuoy1102@gmail.com>
2023-02-07 13:21:36 +0700

    [CTG-141]: Update code

--- scripts/login.js
+++ scripts/login.js
@@ -1,13 +1,13 @@
+let form = document.getElementById("login-form");
 function validateEmail()
     {
-        let form = document.getElementById("login-form");
         let email = document.getElementById("email");
         let text= document.getElementById("text");
         let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
         
         if (email.value.match(emailPattern)){
             form.classList.add("valid");
-            form.classList.remove("invalid")
+            form.classList.remove("invalid");
             text.innerHTML="Valid email!";
             text.style.color="green";
             email.style.border = "2px solid green";
@@ -29,14 +29,13 @@ function validateEmail()
     }
 function validPassoword()
     {
-        let form = document.getElementById("login-form");
         let password = document.getElementById("pass");
         let textPassword= document.getElementById("textPassword");
         if(password.value=="")
         { 
             form.classList.add("valid");
-            form.classList.remove("invalid")
-            textPassword.innerHTML="Please! Enter password.";
+            form.classList.remove("invalid");
+            textPassword.innerHTML="Pleas enter password!";
             textPassword.style.color="red";
             password.style.border = "2px solid red";
             
