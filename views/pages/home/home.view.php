67ed88ef65eb2d2ec943e66e60b4bfb24264b134
ShitaroKhido <khidpagna01@gmail.com>
2023-02-07 15:30:48 +0700

    [CTG-147]: Updated theme and alignment.

--- views/pages/home/home.view.php
+++ views/pages/home/home.view.php
@@ -5,11 +5,18 @@ require("./views/partials/nav.php");
 
 
 <main class="flex">
+    <!-- Side Menu -->
     <?php
-    require("./views/partials/side_menu.php")
+    require("./views/partials/side_menu.php");
     ?>
+    <!-- Cartd Container -->
+    <div class="card-container flex flex-wrap justify-center w-full h-full overflow-scroll overflow-x-hidden gap-8 p-4">
 
+        <?php
+        require("./views/partials/card.php");
+        ?>
 
+    </div>
 
 
 </main>
--- views/partials/side_menu.php
+++ views/partials/side_menu.php
@@ -1,17 +1,17 @@
 <div id="side-menu" class="p-3 relative h-full bg-primary text-secondary shadow-sm shadow-white z-20">
     <div class="flex flex-col w-full space-y-4">
         <!-- Home -->
-        <a href="#" class="flex items-center space-x-6 hover:bg-secondary hover:text-white transition-all duration-200 p-2 rounded-lg">
+        <a href="/" class="flex items-center space-x-6 hover:bg-secondary hover:text-white transition-all duration-200 p-2 rounded-lg">
             <span class="material-symbols-rounded">home</span>
             <span class="side-link-text">Home</span>
         </a>
         <!-- Cart -->
-        <a href="#" class="flex items-center space-x-6 hover:bg-secondary hover:text-white transition-all duration-200 p-2 rounded-lg">
+        <a href="/cart" class="flex items-center space-x-6 hover:bg-secondary hover:text-white transition-all duration-200 p-2 rounded-lg">
             <span class="material-symbols-rounded">shopping_cart</span>
             <span class="side-link-text">Orders</span>
         </a>
         <!-- Settings -->
-        <a href="#" class="flex items-center space-x-6 hover:bg-secondary hover:text-white transition-all duration-200 p-2 rounded-lg">
+        <a href="/settings" class="flex items-center space-x-6 hover:bg-secondary hover:text-white transition-all duration-200 p-2 rounded-lg">
             <span class="material-symbols-rounded">settings</span>
             <span class="side-link-text">Settings</span>
         </a>
