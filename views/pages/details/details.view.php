5d7d78f8140a68277fe11c7510bc2a0a093b07bf
ShitaroKhido <khidpagna01@gmail.com>
2023-02-09 09:23:15 +0700

    [CTG-149]: Updated Code.

--- views/pages/details/details.view.php
+++ views/pages/details/details.view.php
@@ -14,7 +14,7 @@ require("./views/partials/nav.php");
 
         <div class="flex max-sm:flex-col max-sm:items-center w-full text-md font-normal text-white p-4 rounded-lg bg-primary border-2">
             <div class="flex flex-col justify-between h-full w-2/5 max-sm:w-full">
-                <img src="https://picsum.photos/300/200" class="rounded-lg w-[340px] h-[200px] overflow-hidden max-sm:w-full">
+                <img src="https://picsum.photos/300/200" class="rounded-lg w-full h-[200px] overflow-hidden">
                 <ul class="my-4 flex flex-col space-y-2">
                     <li class="flex items-center space-x-2 text-lg font-semibold uppercase"><span><?= $title ?></span></li>
                     <li class="flex items-center space-x-2"><span class="material-symbols-rounded font-light">sell</span><span>Price :</span><span><?= $price ?> $</span></li>
@@ -23,13 +23,13 @@ require("./views/partials/nav.php");
                 <div class="flex flex-col space-y-4 text-black w-full">
                     <div class="flex w-full space-x-2">
                         <select name="date" id="date" class="w-full p-1 rounded-lg">
-                            <option disabled selected>Select</option>
+                            <option disabled selected>Time</option>
                             <option value="">Opt1</option>
                             <option value="">Opt2</option>
                             <option value="">Opt3</option>
                         </select>
                         <select name="Time" id="date" class="w-full p-1 rounded-lg">
-                            <option disabled selected>Select</option>
+                            <option disabled selected>Date</option>
                             <option value="">Opt1</option>
                             <option value="">Opt2</option>
                             <option value="">Opt3</option>
