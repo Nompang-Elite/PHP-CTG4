1bf9f4e67b801ad63f6140b64b67a58a3338da38
ShitaroKhido <khidpagna01@gmail.com>
2023-02-09 09:56:07 +0700

    [CTG-149]: Updated footer style.

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,7 +1,7 @@
 <hr>
-<footer class="flex items-center justify-between bg-primary text-secondary shadow-white w-full text-xs p-2 shadow-xl">
+<footer class="flex items-center justify-between bg-primary text-secondary shadow-white w-full text-xs p-1 shadow-xl">
     <!-- Footer -->
-    <p>TICKETO&copy;Rerserved by Team-4</p>
+    <p>Copyrights &copy; Rerserved by Ticketo Team</p>
     <p>Ver 1.0.0</p>
 </footer>
 </body>
