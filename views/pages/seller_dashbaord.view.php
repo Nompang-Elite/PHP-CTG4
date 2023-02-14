585b285f98b242195f62ca291796c0fe38cf661b
PHANNITPHUOY <phannitphuoy1102@gmail.com>
2023-02-14 20:53:49 +0700

    [CTG-98]: Update code

--- views/pages/seller_dashbaord.view.php
+++ views/pages/seller_dashbaord.view.php
@@ -1,33 +1,35 @@
 <!-- Seller page -->
-<div class="w-full h-full overflow-scroll overflow-x-hidden">
+<div class="w-full h-full overflow-scroll overflow-x-hidden ">
     <div class="text-center text-4xl m-10">
         <h1>Update here!</h1>
     </div>
-    <!-- Button add new ticket -->
-    <div>
-        <button class="bg-yellow-500 rounded-lg p-3 ml-80 mb-4">Add new ticket</button>
-    </div>
-    <!-- Card -->
-    <div class="overflow-scroll overflow-x-hidden">
-    <?php
-    foreach ($shows as $show) :
-    ?>
-        <div class="flex justify-between bg-primary w-2/3 m-auto rounded-lg mb-3 ">
-            <div class="flex">
-                <img class="p-1.5 w-54 rounded-lg h-32" src="data:image/jpeg;base64,<?= base64_encode($show['image']) ?>" alt="" />
-                <div class="m-auto flex gap-20 ml-10 text-white">
-                    <h1 class="font-bold"><?= $show['title'] ;?></h1>
-                    <p><?= $show['price']. " $"; ?></p>
-                    <p><?= $show['datetime']; ?></p>
+    <div >
+        <!-- Button add new ticket -->
+        <div>
+            <button class="bg-yellow-500 rounded-lg p-3 mb-4 ml-40">Add new ticket</button>
+        </div>
+        <!-- Card -->
+        <div class=" overflow-scroll overflow-x-hidden ">
+        <?php
+        foreach ($shows as $show) :
+        ?>
+            <div class="flex justify-between bg-primary w-9/12 m-auto rounded-lg mb-3 ">
+                <div class="flex">
+                    <img class="p-1.5 w-54 rounded-lg h-32" src="data:image/jpeg;base64,<?= base64_encode($show['image']) ?>" alt="" />
+                    <div class="m-auto flex gap-20 ml-10 text-white">
+                        <h1 class="font-bold"><?= $show['title'] ;?></h1>
+                        <p><?= $show['price']. " $"; ?></p>
+                        <p><?= $show['datetime']; ?></p>
+                    </div>
+                </div>
+                <div class="flex items-center mr-10">
+                <span class="material-symbols-outlined text-blue-500" onclick="editShow()" id="<?= $show["id"] ?>">edit_square</span>
+                    <p class="font-bold text-3xl mb-2 p-1 text-white">|</p>
+                    <span class="material-symbols-outlined text-red-700 mt-1 text-2xl" onclick="deleteShow()" id="<?= $show["id"] ?>">delete</span>
                 </div>
             </div>
-            <div class="flex items-center mr-10">
-            <span class="material-symbols-outlined text-blue-500">edit_square</span>
-                <p class="font-bold text-3xl mb-2 p-1 text-white">|</p>
-                <span class="material-symbols-outlined text-red-700 mt-1 text-2xl">delete</span>
-            </div>
+        <?php endforeach ?>
         </div>
-    <?php endforeach ?>
+        <!-- End card -->
     </div>
-    <!-- End card -->
 </div>
