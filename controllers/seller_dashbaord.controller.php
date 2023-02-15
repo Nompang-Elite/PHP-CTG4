bb459d60d23c91224425c8223b83f0ac748def88
ShitaroKhido <khidpagna01@gmail.com>
2023-02-15 19:47:36 +0700

    [CTG-101]: Updated model.

--- controllers/seller_dashbaord.controller.php
+++ controllers/seller_dashbaord.controller.php
@@ -2,6 +2,8 @@
 $config  = require("./utils/config.php");
 // Create new database
 $db = new Database($config['database']['online']);
-
-$shows = getSellerShows($db);
+if ($_SESSION["activeUser"]) {
+    $userId = $_SESSION["activeUser"]["id"];
+    $shows = getSellerShows($db, $userId);
+}
 require("./views/pages/seller_dashbaord.view.php");
--- models/get_seller_ticket.model.php
+++ models/get_seller_ticket.model.php
@@ -1,11 +1,17 @@
 <?php
 // function to get data of shows and return : array
-function getSellerShows(Database $db): array
+function getSellerShows(Database $db, $id): array
 {
     // query to select data of show from database
     $db->query(
         "SELECT * FROM schedules JOIN shows JOIN tickets JOIN venues JOIN images
-        WHERE tickets.id = schedules.ticket_id AND shows.id = tickets.show_id AND venues.id = tickets.venue_id AND shows.image_id = images.id"
+        WHERE tickets.id = schedules.ticket_id AND shows.id = tickets.show_id
+        AND venues.id = tickets.venue_id AND shows.image_id = images.id
+        AND tickets.seller_id = :id
+        ",
+        [
+            ":id" => $id
+        ]
     );
     // return all data of shows
     return $db->getAll();
