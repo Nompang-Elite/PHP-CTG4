18138aa5abbd4aa72da8530cb8cf85276c51ddbf
ShitaroKhido <khidpagna01@gmail.com>
2023-02-16 13:30:41 +0700

    [CTG-147]: Updated dialog.

--- views/pages/form_add_ticket.view.php
+++ views/pages/form_add_ticket.view.php
@@ -1,8 +1,8 @@
-<dialog open id="dialog" class="bg-gray-800 w-full ">
-    <section class=" dark:bg-gray-900">
+<dialog open id="dialog" class="bg-transparent w-full h-full">
+    <section class="-mt-10">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <div class="w-full bg-gray-500 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                <div class="bg-gray-200 p-3 rounded-t-lg">
+            <div class="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-primary dark:border-gray-700">
+                <div class="bg-secondary p-3 rounded-t-lg">
                     <h1 class="text-xl text-center font-bold leading-tight tracking-tight text-gray-800 md:text-2xl dark:text-white">
                         Ticket
                     </h1>
@@ -32,11 +32,11 @@
                             <textarea name="message" id="message" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Message..."></textarea>
                         </div>
                     </div>
-                    <div class="ml-64 mb-5 space-x-1">
-                        <span class="bg-red-500 p-1.5 rounded-lg" onclick="cancel()">
+                    <div class="ml-64 mb-5 flex space-x-1 w-full text-center text-white">
+                        <span class="bg-red-500 p-1.5 rounded-lg w-20" onclick="cancel()">
                             Cancel
                         </span>
-                        <span class="bg-blue-500 p-1.5 rounded-lg w-14" onclick="addTickets()">
+                        <span class="bg-blue-500 p-1.5 rounded-lg w-20" onclick="addTickets()">
                             Add
                         </span>
                     </div>
