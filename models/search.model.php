bd6b7321f4b4c38b42b8f65c6326757373f0e17b
ShitaroKhido <khidpagna01@gmail.com>
2023-02-16 11:04:14 +0700

    [CTG-88]: fixed mismatch search query.

--- models/search.model.php
+++ models/search.model.php
@@ -2,7 +2,7 @@
 function search(Database $db, $input)
 {
     // select data from database
-    $db->query("SELECT  app_db.shows.id, app_db.shows.title,app_db.venues.address, app_db.images.image, app_db.tickets.price, app_db.shows.description 
+    $db->query("SELECT  app_db.shows.title,app_db.venues.address, app_db.images.image, app_db.tickets.price, app_db.tickets.show_id, app_db.shows.description 
     
     FROM shows JOIN tickets JOIN images JOIN venues WHERE shows.id = tickets.show_id AND venues.id = tickets.venue_id AND shows.image_id = images.id AND shows.title LIKE :names ", [":names" =>  "%" . "{$input}" . "%"]);
     // get data from database
